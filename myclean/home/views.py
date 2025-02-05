de28d65179a8c2f252d45520b47e54008ff89481
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-05 22:27:53 +1000

    Add function that renders home.html

--- myclean/home/views.py
+++ myclean/home/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
 def home(request):
     return render(request, 'home/home.html', {})
-=======
-# Create your views here.
->>>>>>> 165eb9b (Create home app)
