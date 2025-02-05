29e25da41664d3b041cc36e3746288ef94406524
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-05 22:27:03 +1000

    Create home app

--- myclean/home/views.py
+++ myclean/home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+<<<<<<< HEAD
 def home(request):
     return render(request, 'home/home.html', {})
+=======
+# Create your views here.
+>>>>>>> 165eb9b (Create home app)
