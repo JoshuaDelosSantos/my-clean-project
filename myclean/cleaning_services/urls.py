bcfbc684cc6b11b061f25e75084e856b5a97f778
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-06 20:15:28 +1000

    Refactor url

--- myclean/cleaning_services/urls.py
+++ myclean/cleaning_services/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cleaning_services', views.cleaning_services, name='cleaning_services'),
+    path('', views.cleaning_services, name='cleaning_services'),
 ]
