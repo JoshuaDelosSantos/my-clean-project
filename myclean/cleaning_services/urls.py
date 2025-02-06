ccff158f25908baeac2244d1d820465db7d5d35f
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-06 19:32:43 +1000

    Add initial url path for cleaning_services app

--- myclean/cleaning_services/urls.py
+++ myclean/cleaning_services/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cleaning_services, name='cleaning_services'),
+    path('cleaning_services', views.cleaning_services, name='cleaning_services'),
 ]
