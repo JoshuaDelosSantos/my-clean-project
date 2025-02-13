f65c740cf68befa8e5cc4b29cd2d7074e5920139
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-13 14:27:00 +1000

    Update view function in urls.py

--- myclean/service_provider/urls.py
+++ myclean/service_provider/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sp_dashboard/', views.service_provider_dashboard, name='sp_dashboard'),
+    path('sp_dashboard/', views.sp_dashboard, name='sp_dashboard'),
 ]
