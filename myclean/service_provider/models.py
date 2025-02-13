7f9520022ef35c4fa80dc507a28bbde26471cd7d
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-13 12:36:03 +1000

    Update model

--- myclean/service_provider/models.py
+++ myclean/service_provider/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class ServiceProvider(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     email = models.EmailField()
     contact_number = models.CharField(max_length=15)
     address = models.TextField()
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    post_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=20)
+    post_code = models.CharField(max_length=4)
     description = models.TextField()
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
