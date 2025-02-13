d8a2210ad4c69a068db3fc420525883c9b720bda
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-13 14:55:43 +1000

    Refactor view function name

--- myclean/accounts/urls.py
+++ myclean/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register_sp/', views.register_service_provider, name='register_sp'),
+    path('register_sp/', views.register_sp, name='register_sp'),
 ]
