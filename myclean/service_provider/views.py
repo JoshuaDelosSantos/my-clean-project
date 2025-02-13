9da5d969473af822f29444e6315398432efd16a1
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-13 14:23:41 +1000

    Add initial service_provider view

--- myclean/service_provider/views.py
+++ myclean/service_provider/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import ServiceProvider
 
-# Create your views here.
+@login_required
+def sp_dashboard(request):
+    """
+    View function for the service provider dashboard.
+    """
+    
+    service_provider = ServiceProvider.objects.get(user=request.user)
+    context = {
+        'service_provider': service_provider
+        }
+    
+    return render(request, 'service_provider/sp_dashboard.html', context)
