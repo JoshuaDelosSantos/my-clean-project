4b148aa022b578094110c8684dd16900297464f4
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-13 12:34:10 +1000

    Initialise service_provider model

--- myclean/service_provider/models.py
+++ myclean/service_provider/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class ServiceProvider(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact_number = models.CharField(max_length=15)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    post_code = models.CharField(max_length=10)
+    description = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
