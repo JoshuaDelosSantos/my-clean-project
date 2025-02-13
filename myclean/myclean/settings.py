94a9aa57f4ab43ca984bad79c512d6a37b3a7627
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-13 11:28:33 +1000

    Remove redundant code

--- myclean/myclean/settings.py
+++ myclean/myclean/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -30,7 +29,6 @@ DEBUG = config('DEBUG')
 
 # TODO: Check before production.
 ALLOWED_HOSTS = ['*']
-SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # Application definition
 
