124f87b7af2d84acc4e88c06369a229f2cfb4308
Jack <jackson.doyle@my.jcu.edu.au>
2025-02-18 15:32:04 +1000

    Added path for login

--- myclean/accounts/urls.py
+++ myclean/accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register_sp/', views.register_sp, name='register_sp'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
