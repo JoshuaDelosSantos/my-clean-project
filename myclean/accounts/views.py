c93499b64ad4ea1654d887a2429d397294839b23
callumjriley3 <callum.riley@my.jcu.edu.au>
2025-02-18 15:25:03 +1000

    Add LoginInterfaceView class.

--- myclean/accounts/views.py
+++ myclean/accounts/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from service_provider.forms import ServiceProviderForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
+
+class LoginInterfaceView(LoginView):
+    # template_name = 'accounts/login.html'
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'accounts/login_sp.html', {"form": form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('sp_dashboard')
+        return render(request, 'accounts/login_sp.html', {"form": form})
 
 
 def register_sp(request):
@@ -28,6 +42,18 @@ def register_sp(request):
         'service_provider_form': service_provider_form
     }
     return render(request, 'accounts/register_sp.html', context)
+
+
+# def login_sp(request):
+#     """
+#     View function for logging in as a service provider.
+#     """
+#     login(request, user)
+#     if:
+#         pass
+#     else:
+#         pass
+
 
 def logout_view(request):
     """
