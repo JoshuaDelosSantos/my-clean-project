06cac4ff9e2d18c89a3cdc9cad36c6e95a4a13b2
Jack <jackson.doyle@my.jcu.edu.au>
2025-02-18 14:08:18 +1000

    add models

--- myclean/service_provider/models.py
+++ myclean/service_provider/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ServiceProvider(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
