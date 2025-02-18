2a75a6df3d377e1c7d286b8edf92ebfba19e51f1
Reece Kelly <reecekelly2004@outlook.com>
2025-02-18 15:37:59 +1000

    Update urls.py to include login_view

--- myclean/accounts/urls.py
+++ myclean/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register_sp/', views.register_sp, name='register_sp'),
     path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login')
 ]
