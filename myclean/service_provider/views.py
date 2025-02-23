f24505ea88b7935f042ba40f21734ee4f5d60bb2
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-02-23 14:00:09 +1000

    Fix import issue

--- myclean/service_provider/views.py
+++ myclean/service_provider/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import ServiceProvider
-from forms import ServiceProviderProfileForm
+from .forms import ServiceProviderProfileForm
 
 @login_required
 def sp_dashboard(request):
