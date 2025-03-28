16e6be14d82628987808481188f1d40d3ae3073c
Jack <jackson.doyle@my.jcu.edu.au>
2025-03-28 16:41:25 +1000

    add new URLs for schedule events

--- myclean/service_provider/urls.py
+++ myclean/service_provider/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('sp_dashboard/', views.sp_dashboard, name='sp_dashboard'),
     path('sp_update_profile/', views.sp_update_profile, name='sp_update_profile'),
+    
+    path('get-schedule/', views.get_schedule, name='get_schedule'),  # For fetching events
+    path('add-schedule/', views.add_schedule, name='add_schedule'),  # For adding new events
+    path('edit-schedule/<int:event_id>/', views.edit_schedule, name='edit_schedule'),  # For editing events
 ]
+
