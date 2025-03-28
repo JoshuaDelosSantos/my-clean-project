4588a4eb333be3b5ed01fb6432f1ca74278f1ebd
Jack <jackson.doyle@my.jcu.edu.au>
2025-03-28 17:34:32 +1000

    fixed something?

--- myclean/service_provider/urls.py
+++ myclean/service_provider/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('sp_dashboard/', views.sp_dashboard, name='sp_dashboard'),
     path('sp_update_profile/', views.sp_update_profile, name='sp_update_profile'),
     
-    path('sp_get_schedule/', views.sp_get_schedule, name='sp_get_schedule'),  # For fetching events
-    path('sp_add_schedule/', views.sp_add_schedule, name='add_schedule'),  # For adding new events
-    path('sp_edit_schedule/<int:event_id>/', views.sp_edit_schedule, name='sp_edit_schedule'),  # For editing events
+    path('sp_get_schedule/', views.get_schedule, name='get_schedule'),  # For fetching events
+    path('sp_add_schedule/', views.add_schedule, name='add_schedule'),  # For adding new events
+    path('sp_edit_schedule/<int:event_id>/', views.edit_schedule, name='edit_schedule'),  # For editing events
 ]
 
